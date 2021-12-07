ca513a7873b54bb1f6b0df8abe8f8067b68887f1
nirmalakurhade <nirmalak19@gmail.com>
2021-12-07 17:54:16 +0530

    AutoIT

--- src/com/jbk/DemoAutoIT.java
+++ src/com/jbk/DemoAutoIT.java
@@ -5,11 +5,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DemoAutoIT {
+	 static WebDriver driver;
 /// used to upload a file into browser
 	public static void main(String[] args) throws Exception {
 		
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		WebDriver driver= new ChromeDriver();
+	    driver= new ChromeDriver();
 		driver.get("file:///E:/AutoIT/Upload.html");
 		driver.manage().window().maximize();
 		driver.findElement(By.id("input")).click();
